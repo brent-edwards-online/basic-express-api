25ec2804c29b5f07c16097d6e87c03c512991464
brent-edwards-online <brent@brentedwardsonline.com>
2017-07-15 09:56:46 +1000

    Much around with pipepline

--- app.js
+++ app.js
@@ -12,20 +12,50 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'jade'); // Template engine
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cors());
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+
 app.use('/users', users);
 app.use('/Tasks', Tasks);
+
+//app.use('/', routes);
+function stuff(req, res, next){
+  console.log('I do stuff');
+  next();
+}
+function morestuff(req, res, next){
+  console.log('I do more stuff');
+  next();
+}
+function log(req, res, next){
+  console.log('I\m a logger and I\m ok');
+  next();
+}
+/* GET home page. */
+app.get('/', stuff, log, function(req, res, next) {
+  res.render('index', { title: 'Ullo govener' });
+});
+
+app.post('/', function(req, res) {
+  res.render('index', { title: 'Ullo govener I\'m posting' });
+});
+
+app.put('/', function(req, res) {
+  res.render('index', { title: 'Ullo govener I\'m putting' });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/Tasks.js
+++ routes/Tasks.js
@@ -1,92 +1,82 @@
 var express = require('express');
 var router = express.Router();
-var Task=require('../models/Task');
+var Task = require('../models/Task');
 
-router.get('/:id?',function(req,res,next){
+router.get('/:id?', function (req, res, next) {
 
-if(req.params.id){
+    if (req.params.id) {
 
-    Task.getTaskById(req.params.id,function(err,rows){
+        Task.getTaskById(req.params.id, function (err, rows) {
 
-        if(err)
-        {
-            res.json(err);
-        }
-        else{
-            res.json(rows);
-        }
-    });
-}
-else{
-
- Task.getAllTasks(function(err,rows){
-
-        if(err)
-        {
-            res.json(err);
-        }
-        else
-        {
-            res.json(rows);
-        }
- 
-    });
-}
-});
-router.post('/',function(req,res,next){
-
-        Task.addTask(req.body,function(err,count){
-
-            console.log(req.body);
-            if(err)
-            {
+            if (err) {
                 res.json(err);
             }
-            else{
-                    res.json(req.body);//or return count for 1 & 0
+            else {
+                res.json(rows);
             }
         });
-});
- router.post('/:id',function(req,res,next){
-  Task.deleteAll(req.body,function(err,count){
-    if(err)
-    {
-      res.json(err);
     }
-    else
-    {
-      res.json(count);
-    }
-  });
-});
-router.delete('/:id',function(req,res,next){
+    else {
 
-        Task.deleteTask(req.params.id,function(err,count){
+        Task.getAllTasks(function (err, rows) {
 
-            if(err)
-            {
+            if (err) {
                 res.json(err);
             }
-            else
-            {
-                res.json(count);
+            else {
+                res.json(rows);
             }
 
         });
+    }
 });
-router.put('/:id',function(req,res,next){
+router.post('/', function (req, res, next) {
+
+    Task.addTask(req.body, function (err, count) {
+
+        console.log(req.body);
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(req.body);//or return count for 1 & 0
+        }
+    });
+});
+router.post('/:id', function (req, res, next) {
+    Task.deleteAll(req.body, function (err, count) {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(count);
+        }
+    });
+});
+router.delete('/:id', function (req, res, next) {
+
+    Task.deleteTask(req.params.id, function (err, count) {
+
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(count);
+        }
+
+    });
+});
+router.put('/:id', function (req, res, next) {
 
     console.log(req.body);
-    Task.updateTask(req.params.id,req.body,function(err,rows){
+    Task.updateTask(req.params.id, req.body, function (err, rows) {
 
-        if(err)
-        {
+        if (err) {
             res.json(err);
         }
-        else
-        {
+        else {
             res.json(rows);
         }
     });
 });
-module.exports=router;
+module.exports = router;
